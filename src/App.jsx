257db08761b54a56254e591f242e81a82d0fa472
safety-crew <153456442+safety-crew@users.noreply.github.com>
2025-11-12 12:58:39 +0200

    Init Repo

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import "./App.css";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your ✈ trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return <li>{item.description}</li>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>🛄 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
+
+function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+export default App;
