e68ab8a3eb12455b10101ca68bd8160d57a8940f
safety-crew <153456442+safety-crew@users.noreply.github.com>
2025-11-14 15:04:23 +0200

    Add Final Features & Organize Components into Separate Files

---
+++ src/components/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
+
+function App() {
+  const [items, setItems] = useState(initialItems);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItems(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList() {
+    if (window.confirm("Are you sure you want to delete all items?")) {
+      setItems([]);
+    }
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItems}
+        onToggleItem={handleToggleItem}
+        handleClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
+
+export default App;
